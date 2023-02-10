4d76bba8b183dc4b74c3931f03531a911aa8d774
Renaud De Pessemier <99337062+3RN0D3P@users.noreply.github.com>
2023-02-10 10:18:32 -0500

    Delete almascript.sh

--- almascript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat ~/.bashrc | egrep "alias ll='ls -l'" || echo "alias ll='ls -l'" >> ~/.bashrc 
-cat ~/.bashrc | egrep "alias la='ls -la'" || echo "alias la='ls -la'" >> ~/.bashrc
-cat ~/.bashrc | egrep "alias l='ls'" || echo "alias l='ls'" >> ~/.bashrc
-
-
-
-
